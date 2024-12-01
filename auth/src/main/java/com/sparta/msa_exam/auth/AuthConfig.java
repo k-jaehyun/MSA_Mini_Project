08c3c0061fab4b0ec820aa246e38270288d980a7
k-jaehyun <wogus146@gmail.com>
2024-12-01 21:55:34 +0900

    project setting: build.gradle, application.yml

---
+++ auth/src/main/java/com/sparta/msa_exam/auth/AuthConfig.java
@@ -0,0 +1,12 @@
+package com.sparta.msa_exam.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class AuthConfig {
+
+}
