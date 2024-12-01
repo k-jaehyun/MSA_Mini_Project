1f29315b53163d3c95a3f9815842978974436e3e
k-jaehyun <wogus146@gmail.com>
2024-12-01 23:58:30 +0900

    ServerPortFilter: 응답 헤더에 서버 포트 추가

---
+++ product/src/main/java/com/sparta/msa_exam/product/filter/ServerPortFilter.java
@@ -0,0 +1,31 @@
+package com.sparta.msa_exam.product.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@WebFilter("/*") // 모든 요청에 대해 필터를 적용
+public class ServerPortFilter implements Filter {
+
+  @Value("${server.port}")
+  private String serverPort;
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+
+    HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+    httpServletResponse.setHeader("Server Port", serverPort); // 헤더에 서버 포트 추가
+
+    chain.doFilter(request, response);
+  }
+
+}
